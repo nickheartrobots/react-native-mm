beaa371faa4a7f53504e0549ab1fc4e96e88a5fd
Nick Clarity <nickc@magenic.com>
2018-02-09 14:57:11 -0600

    Added Login and Home Screen

---
+++ app/Screens/Home/Home.js
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+    Text,
+    View
+} from 'react-native';
+
+export default class Login extends Component {
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.welcome}>
+                    Home Screen
+                </Text>
+            </View>
+        );
+    }
+}
---
+++ app/Screens/Login/Login.js
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+	Text,
+	View
+} from 'react-native';
+import styles from './styles'
+
+export default class Login extends Component {
+	render() {
+		return (
+			<View style={styles.container}>
+				<Text style={styles.welcome}>
+					Login Screen
+        		</Text>
+			</View>
+		);
+	}
+}
